9808b952b75a7273e2408fedc6dec213dc6a48a7
me88hareesh <nharry88@gmail.com>
2023-08-08 22:33:54 +0530

    'adoDotNetEntityDataModel-Added'

--- CQRSMediatRExample/GenericRepository/GenericRepository.cs
+++ CQRSMediatRExample/GenericRepository/GenericRepository.cs
@@ -6,15 +6,19 @@ namespace CQRSMediatRExample.GenericRepository
     public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
         private FakeDataStore? _fakeDataStore = null;
-        public DataSet<T> _dataSet;
+        public DataSet<T>? _dataSet;
+
         public GenericRepository(FakeDataStore _fds) {
             _fakeDataStore = _fds;
+            //_dataSet = _fakeDataStore.Set<T>();
         }
+
         public List<T> GetAll()
         {
-            var listData = _fakeDataStore.GetAllProducts();
+            var listData = _fakeDataStore?.GetAllProducts();
             throw new NotImplementedException();
         }
+
 
         public T GetById(object id)
         {
